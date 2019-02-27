a150f2a53dc69efbb4e15f8f9dfe594b7259f157
DanBonehill <DanBonehill@me.com>
2019-02-27 14:11:52 +0000

    Add Category entity & Many to Many relationship with Recipe

--- src/main/java/com/danbonehill/recipe/domain/Recipe.java
+++ src/main/java/com/danbonehill/recipe/domain/Recipe.java
@@ -30,6 +30,12 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -124,5 +130,13 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
