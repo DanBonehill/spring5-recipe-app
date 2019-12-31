5f45a489abce9b029adf9c86e065beb26ecd5fdc
DanBonehill <DanBonehill@me.com>
2019-12-31 12:32:29 +0000

    Add Update Ingredient Functionality

---
+++ src/main/java/com/danbonehill/recipe/services/UnitOfMeasureService.java
@@ -0,0 +1,10 @@
+package com.danbonehill.recipe.services;
+
+import com.danbonehill.recipe.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
---
+++ src/main/java/com/danbonehill/recipe/services/UnitOfMeasureServiceImpl.java
@@ -0,0 +1,31 @@
+package com.danbonehill.recipe.services;
+
+import com.danbonehill.recipe.commands.UnitOfMeasureCommand;
+import com.danbonehill.recipe.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.danbonehill.recipe.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+
+    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository, UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
+    }
+
+    @Override
+    public Set<UnitOfMeasureCommand> listAllUoms() {
+
+        return StreamSupport.stream(unitOfMeasureRepository.findAll()
+                .spliterator(), false)
+                .map(unitOfMeasureToUnitOfMeasureCommand::convert)
+                .collect(Collectors.toSet());
+    }
+}
